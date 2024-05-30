150def0f53eb9ab216fe04e854da40f66b6fa2cd
Jaylaelike <smarkwisai@gmail.com>
2024-05-30 14:42:01 +0700

    Add DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from 'astro/config';
-import db from '@astrojs/db';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import db from "@astrojs/db";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind({ applyBaseStyles: false })],
-  output: 'server',
-  adapter: cloudflare({
-    imageService: 'compile',
-  }),
+  output: "server",
+  adapter: vercel(),
 });
