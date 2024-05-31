5aea58e02cf9d0c4a0c0daa534e6fb1098cc1483
Jaylaelike <smarkwisai@gmail.com>
2024-05-31 16:58:08 +0700

    Add Analytisc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import webVitals from "@astrojs/web-vitals";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind({ applyBaseStyles: false })],
+  integrations: [db(), tailwind({
+    applyBaseStyles: false
+  }), webVitals()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
